670320abfe147346ab1b834310b96e42ddabdb41
khaled abdelbar <155849984+abdelbar472@users.noreply.github.com>
2025-09-06 20:11:26 +0300

    Update visualization.h with new content

--- visualization.h
+++ visualization.h
@@ -4,9 +4,29 @@
 #include <vector>
 #include <string>
 
-// Forward declare the Plank struct which will be defined in the main file
-// but used in visualization.cpp
-class Plank;
+// Define the PlacedPiece struct
+struct PlacedPiece {
+    int id;
+    int x;
+    int y;
+    int length;
+    int width;
+    bool rotated;
+
+    PlacedPiece(int id, int x, int y, int l, int w, bool r)
+            : id(id), x(x), y(y), length(l), width(w), rotated(r) {}
+};
+
+// Define the Plank class
+class Plank {
+public:
+    std::vector<PlacedPiece> pieces;
+    int usedArea;
+
+    Plank() : usedArea(0) {}
+
+    double getEfficiency() const;
+};
 
 // Generate SVG file visualizing the cutting diagram
 void generateSVG(const std::vector<Plank>& planks, const std::string& filename);
